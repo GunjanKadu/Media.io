a331c2bfae3d7815e6794ce25e1149966666cf02
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-05 02:13:18 +0200

    Core - App Brand Component

---
+++ src/app/core/components/app-brand/app-brand.component.ts
@@ -0,0 +1,27 @@
+import { AppApi } from '@api/app.api';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-brand',
+  styleUrls: ['./app-brand.scss'],
+  template: `
+    <div class="brand-container bg-danger" (click)="toggleSidebar()">
+      <section class="brand-text">
+        <h3 class="text brand-text-item">Med</h3>
+        <icon name="headphones" class="brand-icon brand-text-item"></icon>
+        <h3 class="text brand-text-item">a</h3>
+      </section>
+      <button class="btn btn-transparent sidebar-toggle">
+        <icon name="bars 2x"></icon>
+      </button>
+    </div>
+  `
+})
+export class AppBrandComponent implements OnInit {
+  constructor(private appApi: AppApi) {}
+  ngOnInit() {}
+
+  toggleSidebar() {
+    return this.appApi.toggleSidebar();
+  }
+}
