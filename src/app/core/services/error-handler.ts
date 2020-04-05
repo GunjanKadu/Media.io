5e0afe33a70d3754710a6464bc2c6396c0babbf3
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-05 16:24:20 +0200

    Youtube api error handler service

---
+++ src/app/core/services/error-handler.ts
@@ -0,0 +1,28 @@
+import { ErrorHandler, Injectable, Injector } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { AppApi } from '../api/app.api';
+
+const HandledErrors = ['HttpErrorResponse'];
+const DimissedErrors = ['popup_closed_by_user'];
+const isYoutubeApiError = error =>
+  error && error.error && error.error.error.errors;
+const isString = message => typeof message === 'string';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppErrorHandler implements ErrorHandler {
+  constructor(private injector: Injector) {}
+
+  handleError(error: Error | HttpErrorResponse | any) {
+    const appApi = this.injector.get(AppApi);
+    if (!DimissedErrors.includes(error.name)) {
+      const sanitizedError = isYoutubeApiError(error)
+        ? error.error.error.errors[0]
+        : error;
+      console.error('There was an ERROR:', error);
+      const errorPayload = isString(error) ? { message: error } : error;
+      appApi.notifyError(error);
+    }
+  }
+}
