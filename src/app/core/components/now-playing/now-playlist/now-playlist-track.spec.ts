dec3ed785003e1b92569c9ee54e37bbd04e4b1c5
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-05 17:53:50 +0200

    now playlist track component testing

---
+++ src/app/core/components/now-playing/now-playlist/now-playlist-track.spec.ts
@@ -0,0 +1,50 @@
+import { By } from '@angular/platform-browser';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { PIPES } from '@shared/pipes';
+import { NowPlaylistTrackComponent } from './now-playlist-track.component';
+import {
+  VideoMock,
+  VideoMockWithSpecialChars
+} from '@mocks/now-playlist-track.mocks';
+import { MediaParserService } from '@core/services';
+
+describe('NowPlaylistTrackComponent', () => {
+  let component: NowPlaylistTrackComponent;
+  let fixture: ComponentFixture<NowPlaylistTrackComponent>;
+
+  function createComponent(video = VideoMock) {
+    fixture = TestBed.createComponent(NowPlaylistTrackComponent);
+    component = fixture.componentInstance;
+    component.video = <any>video;
+    fixture.detectChanges();
+  }
+
+  beforeEach(async(() => {
+    const mediaParserSpy = jasmine.createSpyObj('mediaParserSpy', [
+      'extractTracks',
+      'verifyTracksCue',
+      'extractTime',
+      'parseTracks'
+    ]);
+    TestBed.configureTestingModule({
+      declarations: [NowPlaylistTrackComponent, ...PIPES],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [{ provide: MediaParserService, useValue: mediaParserSpy }]
+    }).compileComponents();
+  }));
+
+  it('should create a component', () => {
+    createComponent();
+    expect(component).toBeDefined();
+  });
+
+  it('should select the track when title is clicked', () => {
+    createComponent();
+    const trigger = fixture.debugElement.query(By.css('.video-title'));
+    spyOn(component.select, 'emit');
+    const actual = component.select.emit;
+    trigger.triggerEventHandler('click', {});
+    expect(actual).toHaveBeenCalled();
+  });
+});
