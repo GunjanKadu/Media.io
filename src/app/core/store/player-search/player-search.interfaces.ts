03639ef693fa880a5e7beb81e6b0cf1f845bbdc3
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-05 18:13:17 +0200

    Interfaces need for player search reducer

---
+++ src/app/core/store/player-search/player-search.interfaces.ts
@@ -0,0 +1,42 @@
+export type videoDurations = 'any' | 'long';
+export type videoDefinition = 'any' | 'high';
+export type videoTypes = 'any' | 'movie' | 'episode';
+export interface IQueryParams {
+  preset: string;
+  videoType?: videoTypes;
+  videoDuration?: videoDurations;
+  videoDefinition?: videoDefinition;
+}
+export interface IPlayerSearch {
+  query: string;
+  filter: string;
+  searchType: string;
+  queryParams: any;
+  queryParamsNew: IQueryParams;
+  presets: IPresetParam[];
+  pageToken: {
+    next: string;
+    prev: string;
+  };
+  isSearching: boolean;
+  results: any[];
+}
+
+export interface ISearchQueryParam {
+  [property: string]: any;
+}
+
+export interface IPresetParam {
+  label: string;
+  value: CPresetTypes | string;
+}
+
+export class CSearchTypes {
+  static VIDEO = 'video';
+  static PLAYLIST = 'playlist';
+}
+
+export class CPresetTypes {
+  static FULL_ALBUMS = 'full albums';
+  static LIVE = 'live';
+}
