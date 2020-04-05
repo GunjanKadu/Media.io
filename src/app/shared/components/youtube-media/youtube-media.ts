7964b35913c6a3e53cb1a038755db129d5cd5100
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-05 23:31:16 +0200

    Youtube media logic component

---
+++ src/app/shared/components/youtube-media/youtube-media.ts
@@ -0,0 +1,49 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from '@angular/core';
+import { ICON_PREFIX_BRAND } from '@shared/directives/icon';
+
+@Component({
+  selector: 'youtube-media',
+  styleUrls: ['./youtube-media.scss'],
+  templateUrl: './youtube-media.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class YoutubeMediaComponent {
+  @Input() media: GoogleApiYouTubeVideoResource;
+  @Input() queued = false;
+  @Output() play = new EventEmitter<GoogleApiYouTubeVideoResource>();
+  @Output() queue = new EventEmitter<GoogleApiYouTubeVideoResource>();
+  @Output() add = new EventEmitter<GoogleApiYouTubeVideoResource>();
+  @Output() unqueue = new EventEmitter<GoogleApiYouTubeVideoResource>();
+
+  showDesc = false;
+  isPlaying = false;
+  ICON_PREFIX_BRAND = ICON_PREFIX_BRAND;
+
+  constructor() { }
+
+  playVideo(media: GoogleApiYouTubeVideoResource) {
+    this.play.emit(media);
+  }
+
+  queueVideo(media: GoogleApiYouTubeVideoResource) {
+    this.queue.emit(media);
+  }
+
+  addVideoToPlaylist(media: GoogleApiYouTubeVideoResource) {
+    this.add.emit(media);
+  }
+
+  toggle(showDesc: Boolean) {
+    this.showDesc = !showDesc;
+  }
+
+  removeVideoFromQueue(media: GoogleApiYouTubeVideoResource) {
+    this.unqueue.emit(media);
+  }
+}
