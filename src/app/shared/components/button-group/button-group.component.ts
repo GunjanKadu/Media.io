86a22caf80ba1a2caa6dd4b64da6f03012f540c7
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-05 23:22:06 +0200

    Implementing the button group component

---
+++ src/app/shared/components/button-group/button-group.component.ts
@@ -0,0 +1,44 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewEncapsulation
+} from '@angular/core';
+
+// import './button-group.component.scss';
+
+export interface ButtonGroupButton {
+  label: string;
+  value: any;
+}
+@Component({
+  selector: 'button-group',
+  styleUrls: ['./button-group.component.scss'],
+  template: `
+    <div class="btn-group btn-group-sm navbar-btn">
+      <button [ngClass]="['btn btn-default', 'btn-value-' + button.value, button.value]"
+        *ngFor="let button of buttons"
+        [class.active]="isSelectedButton(button.value)"
+        (click)="buttonClick.next(button)">
+        {{ button.label }}
+      </button>
+    </div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
+})
+export class ButtonGroupComponent implements OnInit {
+  @Input() buttons: ButtonGroupButton[];
+  @Input() selectedButton: string;
+
+  @Output() buttonClick = new EventEmitter<ButtonGroupButton>();
+
+  ngOnInit() { }
+
+  isSelectedButton(buttonValue: string) {
+    return buttonValue === this.selectedButton;
+  }
+}
