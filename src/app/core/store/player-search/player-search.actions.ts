fb32d7f4304afc5196e7a86af6bb388a78dae231
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-05 18:14:00 +0200

    seperating the actions needed for player search reducer

---
+++ src/app/core/store/player-search/player-search.actions.ts
@@ -0,0 +1,131 @@
+import { Injectable } from '@angular/core';
+import { ActionCreatorFactory } from 'ngrx-action-creator-factory';
+import { Action } from '@ngrx/store';
+import { IQueryParams } from './player-search.interfaces';
+
+export enum ActionTypes {
+  ADD = '[Videos] ADD',
+  SET_SELECTED_VIDEO = '[Videos] SET_SELECTED_VIDEO',
+  UPDATE_FILTER = '[PlayerSearch] UPDATE_FILTER',
+  UPDATE_QUERY_PARAM = '[PlayerSearch] UPDATE_QUERY_PARAM',
+  UPDATE_QUERY = '[PlayerSearch] UPDATE_QUERY',
+  SEARCH_NEW_QUERY = '[PlayerSearch] SEARCH_NEW_QUERY',
+  SEARCH_MORE_FOR_QUERY = '[PlayerSearch] SEARCH_MORE_FOR_QUERY',
+  GET_SUGGESTIONS = '[PlayerSearch] GET_SUGGESTIONS',
+  RESET_PAGE_TOKEN = '[PlayerSearch] RESET_PAGE_TOKEN',
+  SEARCH_RESULTS_RETURNED = '[PlayerSearch] SERACH_RESULTS_RETURNED',
+  SEARCH_CURRENT_QUERY = '[PlayerSearch] SEARCH_CURRENT_QUERY',
+  SEARCH_STARTED = '[PlayerSearch] SEARCH_STARTED',
+  SEARCH_TYPE_UPDATE = '[PlayerSearch] SEARCH_TYPE_UPDATE',
+  ADD_PLAYLISTS_TO_RESULTS = '[PlayerSearch] ADD_PLAYLISTS_TO_RESULTS',
+  ADD_METADATA_TO_VIDEOS = '[PlayerSearch] ADD_METADATA_TO_VIDEOS',
+  PLAYLISTS_SEARCH_START = '[PlayerSearch] PLAYLISTS_SEARCH_START',
+  ADD_RESULTS = '[PlayerSearch] ADD_RESULTS',
+  RESET_RESULTS = '[PlayerSearch] RESET_RESULTS',
+  ERROR_RESULTS = '[PlayerSearch] ERROR_RESULTS',
+  UPDATE_QUERY_FILTER = '[PlayerSearch] UPDATE_QUERY_FILTER'
+}
+@Injectable()
+export class PlayerSearchActions {
+  static UPDATE_QUERY_PARAM = '[PlayerSearch] UPDATE_QUERY_PARAM';
+  static UPDATE_QUERY = '[PlayerSearch] UPDATE_QUERY';
+  static SEARCH_NEW_QUERY = '[PlayerSearch] SEARCH_NEW_QUERY';
+  static SEARCH_MORE_FOR_QUERY = '[PlayerSearch] SEARCH_MORE_FOR_QUERY';
+  static GET_SUGGESTIONS = '[PlayerSearch] GET_SUGGESTIONS';
+  static RESET_PAGE_TOKEN = '[PlayerSearch] RESET_PAGE_TOKEN';
+  static SEARCH_RESULTS_RETURNED = '[PlayerSearch] SERACH_RESULTS_RETURNED';
+  static SEARCH_CURRENT_QUERY = '[PlayerSearch] SEARCH_CURRENT_QUERY';
+  static SEARCH_STARTED = '[PlayerSearch] SEARCH_STARTED';
+  static SEARCH_TYPE_UPDATE = '[PlayerSearch] SEARCH_TYPE_UPDATE';
+  static ADD_PLAYLISTS_TO_RESULTS = {
+    action: '[PlayerSearch] ADD_PLAYLISTS_TO_RESULTS',
+    creator: payload => ({
+      payload,
+      type: PlayerSearchActions.ADD_PLAYLISTS_TO_RESULTS.action
+    })
+  };
+
+  static ADD_METADATA_TO_VIDEOS = {
+    action: '[PlayerSearch] ADD_METADATA_TO_VIDEOS',
+    creator: payload => ({
+      payload,
+      type: PlayerSearchActions.ADD_METADATA_TO_VIDEOS.action
+    })
+  };
+
+  static PLAYLISTS_SEARCH_START = {
+    action: '[PlayerSearch] PLAYLISTS_SEARCH_START',
+    creator: () => ({ type: PlayerSearchActions.PLAYLISTS_SEARCH_START.action })
+  };
+
+  // Results Actions
+  static ADD_RESULTS = '[PlayerSearch] ADD_RESULTS';
+  static RESET_RESULTS = '[PlayerSearch] RESET_RESULTS';
+  static ERROR_RESULTS = '[PlayerSearch] ERROR_RESULTS';
+
+  getSuggestions = ActionCreatorFactory.create<string>(
+    PlayerSearchActions.GET_SUGGESTIONS
+  );
+  searchNewQuery = ActionCreatorFactory.create<string>(
+    PlayerSearchActions.SEARCH_NEW_QUERY
+  );
+  searchMoreForQuery = ActionCreatorFactory.create(
+    PlayerSearchActions.SEARCH_MORE_FOR_QUERY
+  );
+  updateFilter = ActionCreatorFactory.create(ActionTypes.UPDATE_FILTER);
+  updateQueryParam = ActionCreatorFactory.create<any>(
+    PlayerSearchActions.UPDATE_QUERY_PARAM
+  );
+  resetPageToken = ActionCreatorFactory.create<any>(
+    PlayerSearchActions.RESET_PAGE_TOKEN
+  );
+  searchResultsReturned = ActionCreatorFactory.create<any>(
+    PlayerSearchActions.SEARCH_RESULTS_RETURNED
+  );
+  searchStarted = ActionCreatorFactory.create(
+    PlayerSearchActions.SEARCH_STARTED
+  );
+  // addResults = ActionCreatorFactory.create(PlayerSearchActions.ADD_RESULTS);
+  resetResults = ActionCreatorFactory.create(PlayerSearchActions.RESET_RESULTS);
+  errorInSearch = ActionCreatorFactory.create<any>(
+    PlayerSearchActions.ERROR_RESULTS
+  );
+}
+
+export class UpdateQueryAction {
+  public type = PlayerSearchActions.UPDATE_QUERY;
+  constructor(public payload: string) {}
+}
+
+export class UpdateSearchType {
+  public type = PlayerSearchActions.SEARCH_TYPE_UPDATE;
+  constructor(public payload: string) {}
+}
+
+export class SearchCurrentQuery {
+  public type = PlayerSearchActions.SEARCH_CURRENT_QUERY;
+  constructor() {}
+}
+
+export class UpdateQueryFilter {
+  public type = ActionTypes.UPDATE_QUERY_FILTER;
+  constructor(public payload: { [key: string]: any }) {}
+}
+// export class AddResults implements Action {
+//   readonly type = PlayerSearchActions.ADD_RESULTS;
+//   constructor(public payload: GoogleApiYouTubeVideoResource[]) { }
+// }
+
+export const AddResultsAction = {
+  type: PlayerSearchActions.ADD_RESULTS,
+  creator(payload: GoogleApiYouTubeVideoResource[]) {
+    return { payload, type: this.type };
+  },
+  handler(state, payload: GoogleApiYouTubeVideoResource[]) {
+    return {
+      ...state,
+      results: [...state.results, ...payload],
+      isSearching: false
+    };
+  }
+};
